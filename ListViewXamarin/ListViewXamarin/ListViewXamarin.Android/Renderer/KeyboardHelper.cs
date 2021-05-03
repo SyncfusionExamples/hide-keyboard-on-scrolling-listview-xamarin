bfccc9b0af8edf7ffa77ed41e1f54dcea04cb11f
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-05-03 16:18:15 +0530

    XAMARIN-34596 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin.Android/Renderer/KeyboardHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+#if Android
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Views.InputMethods;
+using ListViewXamarin.Droid;
+#elif iOS
+using ListViewXamarin.iOS;
+using Xamarin.Forms.Internals;
+using UIKit;
+#endif
+
+[assembly: Dependency(typeof(KeyboardHelper))]
+#if Android
+namespace ListViewXamarin.Droid 
+#elif iOS
+namespace ListViewXamarin.iOS 
+#endif
+{
+    [Preserve(AllMembers = true)]
+    public class KeyboardHelper : IKeyboardHelper
+    {
+        public KeyboardHelper()
+        {
+
+        }
+
+#if Android
+        static Context _context;
+
+        public static void Init(Context context)
+        {
+            _context = context;
+        }
+#endif
+
+        public void HideKeyboard()
+        {
+#if Android
+            var inputMethodManager = _context.GetSystemService(Context.InputMethodService) as InputMethodManager;
+            if (inputMethodManager != null && _context is Activity) 
+            { 
+                var activity = _context as Activity; 
+                var token = activity.CurrentFocus?.WindowToken; 
+                inputMethodManager.HideSoftInputFromWindow(token, HideSoftInputFlags.None); 
+                activity.Window.DecorView.ClearFocus(); 
+            } 
+#elif iOS
+            UIApplication.SharedApplication.KeyWindow.EndEditing(true); 
+#endif
+        }
+    }
+} 
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,58 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    #region Behavior
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+        SfListView ListView;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.ScrollStateChanged += ListView_ScrollStateChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView.ScrollStateChanged -= ListView_ScrollStateChanged;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+        private void ListView_ScrollStateChanged(object sender, ScrollStateChangedEventArgs e)
+        {
+            if (e.ScrollState != ScrollState.Idle)
+            {
+                DependencyService.Get<IKeyboardHelper>().HideKeyboard();
+            }
+        }
+        #endregion
+    }
+    #endregion
+
+    #region IKeyboardHelper
+
+    public interface IKeyboardHelper
+    {
+        void HideKeyboard();
+    }
+    #endregion
+}
